555b123d8a0eeecdd0f326d268e6aca5b56168f1
YagoYJ <yagojuanyjskt@gmail.com>
2019-11-01 13:37:12 -0200

    trocando de bd

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://carteira:minhacarteira123123123@minhacarteira-cw3eh.mongodb.net/test?retryWrites=true&w=majority"
+      "mongodb+srv://CampSkate:campskate@campskate-4wgyg.mongodb.net/test?retryWrites=true&w=majority"
   };
 } else {
   module.exports = {
